ad93abf7b83d35b0d75abbeb824a652ef765176e
Rajat Kaushik <M1046872@mindtree.com>
2020-05-14 23:16:11 +0530

    Initial Commit

---
+++ react-api-data-render-cards/example/src/App.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+import { ExampleComponent } from 'react-api-data-render-cards'
+import 'react-api-data-render-cards/dist/index.css'
+
+const App = () => {
+  return <ExampleComponent text="Create React Library Example 😄" />
+}
+
+export default App
